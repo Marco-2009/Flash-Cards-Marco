5ba770b5f05927da68685439c5320967db64e45a
Marco-2009 <pariz.marco@escola.pr.gov.br>
2025-09-09 08:46:16 -0300

    09/09/25

---
+++ Perguntas.js
@@ -0,0 +1,30 @@
+
+criaCartao(
+    'Qual é a capital da Itália?',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'Qual é o maior mamífero terrestre?',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'Qual cientista formulou a teoria da relatividade?',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
